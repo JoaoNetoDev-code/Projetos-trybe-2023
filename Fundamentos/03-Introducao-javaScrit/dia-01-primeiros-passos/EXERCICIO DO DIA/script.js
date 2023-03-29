45de2809a95e9b474434749bbe6e6b9777e62141
joão neto <jhontdsmit36@gmail.com>
2023-03-29 20:45:09 -0300

    exercicio 6 feito

--- Fundamentos/03-Introducao-javaScrit/dia-01-primeiros-passos/EXERCICIO DO DIA/script.js
+++ Fundamentos/03-Introducao-javaScrit/dia-01-primeiros-passos/EXERCICIO DO DIA/script.js
@@ -73,28 +73,48 @@
 
 //////////////////////////////////////////////////////////////////////////
 
-let lado1 = 80;
-let lado2 = 70;
-let lado3 = 30;
-let soma = lado1 + lado2 + lado3;
-let resultado;
+// let lado1 = 80;
+// let lado2 = 70;
+// let lado3 = 30;
+// let soma = lado1 + lado2 + lado3;
+// let resultado;
 
 
-if ( soma === 180) 
-{
-    resultado = true
-}
-    else if (soma < 180) 
-{
-    resultado = false;
-}
-    else if (soma > 180)
-{
-    resultado = false;
-}
-else 
-{
-    resultado = 'Erro!';
-}
+// if ( soma === 180) 
+// {
+//     resultado = true
+// }
+//     else if (soma < 180) 
+// {
+//     resultado = false;
+// }
+//     else if (soma > 180)
+// {
+//     resultado = false;
+// }
+// else 
+// {
+//     resultado = 'Erro!';
+// }
 
-console.log(resultado) 
+// console.log(resultado) 
+
+/////////////////////////////////////////////
+
+let rei = 'move-se para todas as direções pela vertical, horizontal ou diagonal, mas apenas uma casa por lance.'
+let rainha ='pode mover-se qualquer número de casas em linha reta - verticalmente, horizontalmente ou diagonalmente'
+let cavalo = 'move-se duas casas na horizontal ou na vertical e depois uma para o lado. O movimento completo do cavalo forma um «L» sobre o tabuleiro.'
+
+let peça ='cavalo'
+
+switch (peça) {
+            case 'rei':
+            console.log(' O Rei move-se para todas as direções pela vertical, horizontal ou diagonal, mas apenas uma casa por lance.');
+            break
+            case 'rainha':
+            console.log('A Rainha pode mover-se qualquer número de casas em linha reta - verticalmente, horizontalmente ou diagonalmente')
+            break
+            case 'cavalo':
+            console.log('O Cavalo move-se duas casas na horizontal ou na vertical e depois uma para o lado. O movimento completo do cavalo forma um «L» sobre o tabuleiro.')
+             default: console.log('Desculpe sua peça não foi registrada.')
+            }
