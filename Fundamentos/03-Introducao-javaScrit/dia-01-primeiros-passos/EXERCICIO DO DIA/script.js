2f3d23e71c9911adfc28b9f8e60aa7441ce01bce
joão neto <jhontdsmit36@gmail.com>
2023-03-29 18:41:05 -0300

    atividade do dia

---
+++ Fundamentos/03-Introducao-javaScrit/dia-01-primeiros-passos/EXERCICIO DO DIA/script.js
@@ -0,0 +1,56 @@
+// let a = 4;
+// let b = 3;
+// let resultado;
+
+// resultado = a + b;
+//  console.log(resultado );
+
+//  resultado = a - b;
+//  console.log(resultado)
+
+//  resultado = b * a;
+//  console.log(resultado)
+
+//  resultado = a / b;
+//  console.log(resultado)
+ 
+//  resultado = a ** b;
+//  console.log(resultado)
+
+ ///////////////////////////////////////////
+
+// let leite = 5
+// let pão = 1
+// let comentario;
+
+//  if (leite > pão) {
+//     comentario = 'leite  é mais caro que pão';
+//  } else if (pão > leite) {
+//     comentario = 'pão é mais caro que leite!';
+// }
+// else {
+//     comentario = 'ambos tem o mesmo preço!';
+// } console.log(comentario)
+
+////////////////////////////////////////////////
+
+let numero3 = 15
+let numero2= 13
+let numero1 = 22
+
+if (numero3 > numero2 && numero3 > numero1) 
+{
+    console.log(numero3 + ' é o maior numero');
+}
+
+else if (numero2 > numero3 && numero2 > numero1) 
+{
+    console.log(numero2 + ' é o maior numero');
+}
+
+else 
+{
+    console.log(numero1 + ' é o maior numero');
+}
+
+
