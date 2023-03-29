e718b109279a7e60a4b66d49c1e7c2634685b139
joão neto <jhontdsmit36@gmail.com>
2023-03-29 15:46:30 -0300

    atividades do dia

---
+++ Fundamentos/03-Introducao-javaScrit/dia-01-primeiros-passos/ATIVIDADE PROPOSTA 06/script.js
@@ -0,0 +1,20 @@
+// let bebidaPrincipal =  'café';
+// let bebidaAlternativa = 'suco de laranja';
+
+// if (bebidaPrincipal === 'café' || bebidaAlternativa === 'suco de laranja') {
+//   console.log("Obrigado(a) por me atender :D")
+// } else {
+//   console.log("Ei, eu não pedi isso!");
+// }
+// console.log(true || true); // true
+// console.log(true || false); // true
+// console.log(false || true); // true
+// console.log(false || false); // false
+
+let weekDay ='quarta-feira'
+
+if (weekDay === 'segunda-feira'|| weekDay === 'quarta-feira' || weekDay ==='quinta-feira' || weekDay === 'sexta-feira') {
+    console.log('Oba, mais um dia de aprendizado na trybe:D');
+}
+else {console.log('FINALMENTE, descanso merecido UwU!');
+}
