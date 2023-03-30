73e3b3cf0dc569f48f9d46081dd22ab0e16140c5
joão neto <jhontdsmit36@gmail.com>
2023-03-30 00:52:50 -0300

    conteudo do dia

---
+++ Fundamentos/03-Introducao-javaScrit/dia-02-array-loop-for/CONTEUDOS DO DIA/script.js
@@ -0,0 +1,7 @@
+let nomePessoa1 = "João";
+let sobrenomePessoa1 = "Nascimento";
+let nomePessoa2 = "Renan";
+let sobrenomePessoa2 = "Fernandes";
+
+// Gerando uma variável com nome e sobrenome juntos:
+let nomeCompletoPessoa1 = nomePessoa1 + " " + sobrenomePessoa1;
