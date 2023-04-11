bbb63d13296b68a2ca29f7bc9b338e7ace0e3c5c
joão neto <jhontdsmit36@gmail.com>
2023-04-11 11:23:41 -0300

    atividade do dia

---
+++ Fundamentos/04 - javascript: DOM eventos e web storage/dia01: DOM e seletores/getElementById/script.js
@@ -0,0 +1,9 @@
+console.log('trybe')
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById('page-title').innerText = 'Trybe em Desenvolvimento';
+
+document.getElementById('paragraph').style.color = 'blue';
+
+document.getElementById('subtitle').innerText = 'papo serio kk'
