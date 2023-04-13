d28a59d3cfa145ffe92b1da0f89e9d6b2f0d62c9
João Neto Turma 32 - A <joao@joao-neto-turma-32-a.local>
2023-04-12 22:33:08 -0300

    tarafa do dia

--- Fundamentos/04 - javascript: DOM eventos e web storage/dia01: DOM e seletores/getElementById/script.js
+++ Fundamentos/04 - javascript: DOM eventos e web storage/dia01: DOM e seletores/getElementById/script.js
@@ -1,9 +1,14 @@
-console.log('trybe')
 const paragraph = document.getElementById("paragraph");
-paragraph.style.color = "red";
+      paragraph.style.color = "red";
 
-document.getElementById('page-title').innerText = 'Trybe em Desenvolvimento';
+const title = document.getElementById('page-title');
+    title.innerText = 'sherek';
+    title.style.color = 'green'
 
-document.getElementById('paragraph').style.color = 'blue';
+const subtitle = document.getElementById('subtitle');
+    subtitle.innerText = 'todo mundo sabe disso >..';
+    subtitle.style.color =  'green';
 
-document.getElementById('subtitle').innerText = 'papo serio kk'
+const secondParagraph = document.getElementById('second-paragraph');
+    secondParagraph.innerText = ' papo serio man... não tem melhor !';
+    secondParagraph.style.color = 'red';
