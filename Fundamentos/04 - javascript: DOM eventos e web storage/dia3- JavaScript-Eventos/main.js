44fbe16970b737f2bc81bb077d2c7d86ce4ec992
João Neto Turma 32 - A <joao@turma-32-a.local>
2023-04-16 00:57:08 -0300

    atividade do dia 3 feita

--- Fundamentos/04 - javascript: DOM eventos e web storage/dia3- JavaScript-Eventos/main.js
+++ Fundamentos/04 - javascript: DOM eventos e web storage/dia3- JavaScript-Eventos/main.js
@@ -10,17 +10,39 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando este for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como é possível fazer isso? Dica: Lembre-se do método `.classList.remove`.
+const addClass = (event) => {
+    const techElement = document.querySelector('.tech');
+    techElement.classList.remove('tech');
+    event.target.classList.add('tech');
+    imput.value = '';
+}
 
+firstLi.addEventListener('click', addClass);
+secondLi.addEventListener('click', addClass);
+thirdLi.addEventListener('click', addClass);
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech'.
+
+input.addEventListener('input', (Event) => {
+    const techElement = document.querySelector('.tech');
+    techElement.innerText = Event.target.value;
+});
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy',
 // redirecione para alguma página.
 // 4.1. Que tal redirecionar para seu portfólio? Dica: Lembre-se dos métodos `window.location.replace` e `window.open`.
-
+myWebpage.addEventListener('dblclick', () =>{
+    window.location.replace('https://blog.betrybe.com/');
+});
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo.
+myWebpage.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'green';
+});
 
+myWebpage.addEventListener('mouseout', (Event) => {
+    Event.target.style.color = 'unset';
+});
 // Segue abaixo um exemplo de uso do event.target.
 
 const resetText = (event) => {
