981d17ce0e6d3ff8aadc28d8f40dcef192bcf528
João Neto Turma 32 - A <joao@turma-32-a.local>
2023-04-27 21:09:52 -0300

    atividade   proposta 2  feita

--- Fundamentos/06 - Introdução à JavaScript ES6 e Testes Unitários/dia1/conteudo 2/script.js
+++ Fundamentos/06 - Introdução à JavaScript ES6 e Testes Unitários/dia1/conteudo 2/script.js
@@ -1,3 +1,24 @@
-const checkValidRange= (numero) => {
+const checkValidRange= (value1,value2,value3,value4) => {
+    
+    const checkNumber = (value1,value2,value3,value4) => {
 
+      if ( isNaN(value1),isNaN(value2),isNaN(value3),isNaN(value4)) {
+        throw new Error('só é possivel calcular a média com numeros!')
+    }
+
+    }
+    
+    try{
+    checkNumber(value1,value2,value3,value4)
+
+    const media = value1 + value2 + value3 + value4 / 4;
+    return media;
+
+    } catch(error) {
+     return error.message;
+    }
+    
+
+ 
 }
+console.log(checkValidRange(2,2,2,2));
