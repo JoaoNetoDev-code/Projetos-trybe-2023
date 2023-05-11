fcd70a42bea719721b9e34021feed654b1048ee5
João Neto Turma 32 - A <joao@turma-32-a.local>
2023-05-11 19:49:27 -0300

    att de hoje

--- Fundamentos/07 - Higher Order Functions do JavaScript ES6/dia 3/atividade proposta/para fixar2.js
+++ Fundamentos/07 - Higher Order Functions do JavaScript ES6/dia 3/atividade proposta/para fixar2.js
@@ -100,11 +100,22 @@ countA();
 // O index 0 do array `students` equivale ao index 0 do array `grades`
 const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
 const expectedResult2 = [
  { name: 'Pedro Henrique', average: 7.8 },
  { name: 'Miguel', average: 9.2 },
  { name: 'Maria Clara', average: 8.8 },
 ];
+
 const studentAverage = () => {
-  // retorne seu código aqui
+    const estudante = students.map((aluno, index) => (
+        {
+            name: aluno,
+            average:grades[index].reduce((acc, curr) => acc + curr)/grades[index].length
+        }
+    )
+    );
+    return estudante;
+    
 }
+console.log(studentAverage())
