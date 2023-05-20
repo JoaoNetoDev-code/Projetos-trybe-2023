04c36398e87b973a806010a1bbc81f03ea135f69
João Neto Turma 32 - A <joao@turma-32-a.local>
2023-05-19 21:01:24 -0300

    validador de campos feito

---
+++ Front-End/01- Introdução ao Front-Ent e JavaCript assincrono/dia-01/validador-de-campos/src/main.js
@@ -0,0 +1,22 @@
+import validator from 'validator';
+
+const verificar = validator;
+const imputText = document.querySelector('#value');
+const buttonValidator = document.querySelector('#button');
+const imputTypeValidator = document.querySelector('#option');
+const textExit = document.querySelector('#textoDeSaida');
+const UUID_VERSION = 4;
+
+buttonValidator.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const campos = {
+    cpf: verificar.isTaxID(imputText.value, 'pt-BR'),
+    hexColor: verificar.isHexColor(imputText.value),
+    email: verificar.isEmail(imputText.value),
+    uuid: verificar.isUUID(imputText.value, UUID_VERSION),
+    url: verificar.isURL(imputText.value),
+  };
+
+  textExit.innerHTML = `A validação retornou ${campos[imputTypeValidator.value]}`;
+});
