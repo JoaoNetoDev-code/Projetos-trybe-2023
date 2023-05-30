977bf30a90f8fd1a33a8590fa6f223b398d46f33
João Neto Turma 32 - A <joao@turma-32-a.local>
2023-05-30 00:21:10 -0300

    requisitos feitos dia 3 concluido

--- Front-End/01- Introdução ao Front-Ent e JavaCript assincrono/dia-03/src/main.js
+++ Front-End/01- Introdução ao Front-Ent e JavaCript assincrono/dia-03/src/main.js
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2'
+
 import { renderCoins } from './components';
 import { fethExchange } from './services/exchange';
 
@@ -5,12 +7,24 @@ import './style.css';
 import './reset.style.css';
 const btnElement = document.querySelector('header form button');
 
-btnElement.addEventListener('click', () => {
+btnElement.addEventListener('click', (event) => {
     const inputElement = document.querySelector('header form input');
-    const inputValue = inputElement.value;
-    fethExchange(inputValue).then((data) => {
-        const rates = data.rates;
+    const inputValue = inputElement.value.toUpperCase();
+
+    if(!inputValue) {
+        Swal.fire({
+            title: 'Erro!',
+            text: 'Você Precisa Digitar uma Moeda',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+          })
+          return;
+    }
+    fethExchange(inputValue)
+        .then((data) => {
         const base = data.base;
+        const rates = data.rates;
+
         const ratesArray = Object.entries(rates)
         
         const objetoFinal = ratesArray.map((rateCoin) => {
@@ -21,5 +35,12 @@ btnElement.addEventListener('click', () => {
             }
         })
         renderCoins(objetoFinal, base)
-    });
+    }).catch (error => {
+        Swal.fire({
+            title: 'Erro!',
+            text: error.message,
+            icon: 'error',
+            confirmButtonText: 'Ok'
+          })
+    })
 })
