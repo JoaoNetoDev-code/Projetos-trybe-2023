441e0a2556dacbe190ed71fc482c52b60b55faf5
João Neto Turma 32 - A <joao@turma-32-a.local>
2023-06-13 21:53:36 -0300

    atividade do dia 2

--- Front-End/03 -  Aprofundando no React/dia 02/aprendendo useEffect/src/App.tsx
+++ Front-End/03 -  Aprofundando no React/dia 02/aprendendo useEffect/src/App.tsx
@@ -1,16 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import fetchCoordinates from './services';
+import Coordinates from './components/Coordinates';
+import ISSLocation from './components/ISSLocation';
+
+import './App.css';
+
+type Location = {
+  longitude: number;
+  latitude: number;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [issLocation, setIssLocation] = useState<Location>({
+    longitude: 0,
+    latitude: 0,
+  });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function setLocation() {
+      const location = await fetchCoordinates();
+      setIssLocation(location);
+      setLoading(false);
+    }
+
+    if (loading) {
+      setLocation();
+    }
+
+    const intervalId = setInterval(() => {
+      setLocation();
+    }, 3000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  if (loading) return <h1>Loading...</h1>;
 
   return (
-    <>
-    
-    </>
-  )
+    <div className="App">
+      <h1>International Space Station Location Tracker</h1>
+      <Coordinates
+        latitude={issLocation.latitude}
+        longitude={issLocation.longitude}
+      />
+      <ISSLocation
+        latitude={issLocation.latitude}
+        longitude={issLocation.longitude}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
